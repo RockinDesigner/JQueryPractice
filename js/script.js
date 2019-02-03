6db227613de0190b328ba70a7bc3f4653c46bb58
RockinDesigner <43171022+RockinDesigner@users.noreply.github.com>
2019-02-04 01:10:21 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $(document).on("mousemove", function(ev) {
+    var mouseX = ev.originalEvent.pageX
+    var mouseY = ev.originalEvent.pageY
+    $("img").each(function() {
+      var imgX = $(this).position().left + 70
+      var imgY = $(this).position().top + 70
+
+      var diffX = mouseX - imgX
+      var diffY = mouseY - imgY
+
+      var radians = Math.atan2(diffY, diffX)
+      var angle = radians * 180 / Math.PI
+
+      $(this).css("transform", "rotate("+ angle + "deg)")
+    });
+    var count = 0
+    var images = ["img/1.png", "img/2.png", "img/3.png"]
+
+    setInterval(function() {
+      count += 1
+      var image = images[count]
+
+      $("img").attr("src", image)
+
+    }, 2000)
+
+  });
+});
